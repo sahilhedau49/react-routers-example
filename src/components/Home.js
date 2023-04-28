ea2debfcbdaf854b726046f356fef16dab09ad10
sahilhedau49 <hedausahil49@gmail.com>
2023-04-28 23:09:14 +0530

    Tailwind CSS added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,18 @@ import React from "react";
 
 const Home = () => {
   return (
-    <>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Connect</li>
+    <div className="bg-red-500 min-h-screen px-12 py-12">
+      <div className="text-lg flex w-full justify-between">
+        <h2 className="font-bold">Logo</h2>
+        <ul className="w-72 flex justify-between  ">
+          <li className="hover:text-white transition">Home</li>
+          <li className="hover:text-white transition">About</li>
+          <li className="hover:text-white transition">Contact</li>
+          <li className="hover:text-white transition">Connect</li>
         </ul>
       </div>
-      <h1>Wellcome to home page.</h1>
-    </>
+      <h1 className="my-8 text-2xl font-bold">Wellcome to home page.</h1>
+    </div>
   );
 };
 
